61211f4e63511a091d85f4d860c69cd627fb5c25
deepak-hce <47603898+deepak-hce@users.noreply.github.com>
2020-09-06 10:29:34 +0530

    mondgo done

--- index.js
+++ index.js
@@ -15,7 +15,11 @@ mongoose.Promise = Promise;
 
 // connect to mongo db
 const mongoUri = config.mongo.host;
-mongoose.connect(mongoUri, { server: { socketOptions: { keepAlive: 1 } } });
+
+console.log(mongoUri);
+
+mongoose.connect(mongoUri,{ useNewUrlParser: true, server: { socketOptions: { keepAlive: 1 } } });
+
 mongoose.connection.on('error', () => {
   throw new Error(`unable to connect to database: ${mongoUri}`);
 });
